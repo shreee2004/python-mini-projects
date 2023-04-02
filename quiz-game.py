0d987d1f970095dea4edcdfae46b769e35d3522e
shreee2004 <shreepadalekar@gmail.com>
2023-04-02 14:34:28 +0530

    not yes

--- quiz-game.py
+++ quiz-game.py
@@ -36,4 +36,4 @@ else:
     print("Incorrect!")
 
 print("You got " + str(score) + " questions correct! ")
-print("You got " + str((score/4) * 100) + "%.yes")
+print("You got " + str((score/4) * 100) + "%.")
