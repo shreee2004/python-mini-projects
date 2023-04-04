5b6d317d4fcdedcd3eb1415b4a1428f99f5b8839
shreee2004 <shreepadalekar@gmail.com>
2023-04-04 15:48:18 +0530

    Check

--- number-gusser.py
+++ number-gusser.py
@@ -14,3 +14,11 @@ else:
 
 random_number = random.randint(0, top_of_range)
 print(random_number)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        quit()
