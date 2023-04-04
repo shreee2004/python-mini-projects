e2025443b7f8d6991fee7a0c8875810d9b6c856d
shreee2004 <shreepadalekar@gmail.com>
2023-04-04 16:02:17 +0530

    Guess complete

--- number-gusser.py
+++ number-gusser.py
@@ -13,7 +13,6 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
-print(random_number)
 
 while True:
     user_guess = input("Make a guess: ")
@@ -21,4 +20,10 @@ while True:
         user_guess = int(user_guess)
     else:
         print('Please type a number next time.')
-        quit()
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong!")
